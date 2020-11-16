337becf5e395e9a150199ddee2272a19f682b7be
Alexander wood <acegoal07@gmail.com>
2020-11-16 12:10:11 +0000

    update

--- Resources/JS/elevator-settings.js
+++ Resources/JS/elevator-settings.js
@@ -1,6 +1,17 @@
 window.onload = function() {
     var elevator = new Elevator({
       element: document.querySelector('.elevator-button'),
-      duration: 1000 // milliseconds
+      duration: 1000, // milliseconds
     });
-  }
+    var elevator = new Elevator({
+      element: document.querySelector('.elevator-admin'),
+      targetElement: document.querySelector('#admin'),
+      verticalPadding: 100  // in pixels
+    });
+    var elevator = new Elevator({
+      element: document.querySelector('.elevator-general'),
+      targetElement: document.querySelector('#general'),
+      verticalPadding: 100  // in pixels
+    }); 
+  };
+
