bf81caf0ace7ff1bfb1a516e6e7df3838d448893
Alexander wood <acegoal07@gmail.com>
2020-11-21 15:29:57 +0000

    update

--- Projects/Surprise/clock.js
+++ Projects/Surprise/clock.js
@@ -1,18 +1,20 @@
 var countDownDate = new Date("nov 27, 2020 00:00:00").getTime();
 var x = setInterval(function() {
+var clock = document.getElementById("clock");
+var clock_text = document.getElementById("clock-text");
   var now = new Date().getTime();
   var distance = countDownDate - now;
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("clock").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  clock.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "; 
+  clock_text.innerHTML = "your will have to wait";
   if (distance < 0) {
     clearInterval(x);
-    var link = document.getElementById("clock");
-    link.classList.add("card-link");
-    link.innerHTML = ":)";
-    link.href = "https://acegoal07.github.io/Projects/Surprise/secret";
+    clock_text.innerHTML = "well the day has come";
+    clock.classList.add("card-link");
+    clock.innerHTML = ":)";
+    clock.href = "https://acegoal07.github.io/Projects/Surprise/secret";
   }
 }, 1000);
