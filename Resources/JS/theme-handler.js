0372769d09ccda1574a45363f807f51375c70929
Alexander wood <acegoal07@gmail.com>
2021-05-16 21:21:28 +0100

    Update theme-handler.js

--- Resources/JS/theme-handler.js
+++ Resources/JS/theme-handler.js
@@ -1,6 +1,6 @@
 var theme = document.getElementById('theme');
 const currentTheme = localStorage.getItem("setTheme");
-var a = document.getElementById('base')
+var base = document.getElementById('base')
   
 function toggleTheme() {
     if (theme.getAttribute('href') == '/Resources/CSS/theme/light.css') {
@@ -16,19 +16,19 @@ if (currentTheme == ('')) {
     if (event.matches) {
         theme.setAttribute('href', '/Resources/CSS/theme/dark.css');
         localStorage.setItem("setTheme", "dark");
-        a.classList.remove("preload");
+        base.classList.remove("preload");
     } else {
         theme.setAttribute('href', '/Resources/CSS/theme/light.css');
         localStorage.setItem("setTheme", "light");
-        a.classList.remove("preload");
+        base.classList.remove("preload");
     }
 })
 } if (currentTheme == "dark") {
     theme.setAttribute('href', '/Resources/CSS/theme/dark.css');
-    a.classList.remove("preload");
+    base.classList.remove("preload");
 } else {
     theme.setAttribute('href', '/Resources/CSS/theme/light.css');
-    a.classList.remove("preload");
+    base.classList.remove("preload");
 }
 
   
