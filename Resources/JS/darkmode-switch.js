e2fe6ed6a81521261c4945b1c6df98abe814483e
Alexander wood <acegoal07@gmail.com>
2021-05-16 17:07:24 +0100

    Update darkmode-switch.js

--- Resources/JS/darkmode-switch.js
+++ Resources/JS/darkmode-switch.js
@@ -1,32 +1,34 @@
 var theme = document.getElementById('theme');
+/*const storage = localStorage.setItem("setTheme", "no-theme");*/
 
-const currentTheme = localStorage.getItem("setTheme").setItem("null");
+const currentTheme = localStorage.getItem("setTheme");
+
 if (currentTheme == "dark") {
     theme.setAttribute('href', 'Resources/CSS/theme/dark.css');
 }
 
 function toggleTheme() {
-    let setTheme = "light"
+    let currentTheme = "light"
     // Change the value of href attribute 
     // to change the css sheet.
     if (theme.getAttribute('href') == 'Resources/CSS/theme/light.css') {
         theme.setAttribute('href', 'Resources/CSS/theme/dark.css');
-        setTheme = "dark";
+        currentTheme = "dark";
     } else {
         theme.setAttribute('href', 'Resources/CSS/theme/light.css');
-        setTheme = "light"
+        currentTheme = "light"
     }
 }
 
-if (currentTheme == ('null')) {
+if (currentTheme == ('')) {
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-    let setTheme = "light"
+    let currentTheme = "light"
     if (event.matches) {
         theme.setAttribute('href', 'Resources/CSS/theme/dark.css');
-        setTheme = "dark";
+        currentTheme = "dark";
     } else {
         theme.setAttribute('href', 'Resources/CSS/theme/light.css');
-        setTheme = "light"
+        currentTheme = "light"
     }
 })
 } if (currentTheme == "dark") {
