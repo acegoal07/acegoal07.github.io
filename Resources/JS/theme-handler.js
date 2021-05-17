ddc721ffefa51da020ce161394c172e4e5adb7fd
Alexander wood <acegoal07@gmail.com>
2021-05-17 15:35:27 +0100

    theme-handler fix

--- Resources/JS/theme-handler.js
+++ Resources/JS/theme-handler.js
@@ -1,5 +1,5 @@
 var theme = document.getElementById('theme');
-const currentTheme = localStorage.getItem('setTheme');
+const currentTheme = localStorage.getItem("setTheme");
 var base = document.getElementById('base')
   
 function toggleTheme() {
@@ -11,27 +11,19 @@ function toggleTheme() {
         localStorage.setItem("setTheme", "light");
     }
 }
-
-if (localStorage.getItem("setTheme") === null) {
-    console.log("Hello World!")
+if (currentTheme === null) {
     if (window.matchMedia('prefers-color-scheme: dark').matches) {
-        console.log("detected system prefers dark mode")
         theme.setAttribute('href', '/Resources/CSS/theme/dark.css');
         localStorage.setItem("setTheme", "dark");
         base.classList.remove("preload");
     } else {
-        console.log("detected system prefers light mode")
         theme.setAttribute('href', '/Resources/CSS/theme/light.css');
         localStorage.setItem("setTheme", "light");
         base.classList.remove("preload");
     }
 }
-
 if (currentTheme == "dark") {
     theme.setAttribute('href', '/Resources/CSS/theme/dark.css');
-    base.classList.remove("preload"); 
-} if (currentTheme == "green") {
-    theme.setAttribute('href', '/Resources/CSS/theme/green.css');
     base.classList.remove("preload");
 } else {
     theme.setAttribute('href', '/Resources/CSS/theme/light.css');
