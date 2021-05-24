70626fcea4e2de28ef2baeba21630ad0184884b9
Alexander wood <acegoal07@gmail.com>
2021-05-24 21:07:29 +0100

    update

---
+++ Resources/JS/theme-handler-cookie-test.js
@@ -0,0 +1,38 @@
+const username = getCookie("set-theme");
+username = "set-theme=test; path=/";
+
+const currentTheme = localStorage.getItem("setTheme");
+
+const lightTheme = '/Resources/CSS/theme/light.css';
+const darkTheme = '/Resources/CSS/theme/dark.css';
+var link = document.getElementById('theme');
+    if (currentTheme == 'dark') {
+        link.href = darkTheme;
+    } else {
+        link.href = lightTheme;
+    }
+    document.head.appendChild(link);
+    document.onreadystatechange = () => {
+        if (document.readyState === 'complete') {
+            document.getElementById('base').classList.remove('disable-transitions');
+        }
+    }
+var theme = document.getElementById('theme');
+    if (currentTheme === null) {
+        if (window.matchMedia('prefers-color-scheme: dark').matches) {
+            theme.setAttribute('href', darkTheme);
+            localStorage.setItem("setTheme", "dark");
+        } else {
+            theme.setAttribute('href', lightTheme);
+            localStorage.setItem("setTheme", "light");
+        }
+    }
+function toggleTheme() {
+    if (theme.getAttribute('href') == lightTheme) {
+        theme.setAttribute('href', darkTheme);
+        localStorage.setItem("setTheme", "dark");
+    } else {
+        theme.setAttribute('href', lightTheme);
+        localStorage.setItem("setTheme", "light");
+    }
+}
