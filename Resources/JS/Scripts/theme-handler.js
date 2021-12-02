cc2b39a44629615ff36381b0f3d3cf6d2e8fdc41
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2021-12-02 23:32:33 +0000

    Update theme-handler.js

--- Resources/JS/Scripts/theme-handler.js
+++ Resources/JS/Scripts/theme-handler.js
@@ -41,24 +41,6 @@ const darkTheme = '/Resources/CSS/theme/dark.css';
                 }
             }
         }
-    var theme = document.getElementById('theme');
-        if (currentTheme === null) {
-            if (window.matchMedia('prefers-color-scheme: dark').matches) {
-                theme.setAttribute('href', darkTheme);
-                localStorage.setItem("setTheme", "dark");
-                document.getElementById('themeSwitch').classList.remove("bi-moon-fill");
-                document.getElementById('themeSwitch').classList.add("bi-brightness-high-fill");
-                document.getElementById('colThemeSwitch').classList.remove("bi-moon-fill");
-                document.getElementById('colThemeSwitch').classList.add("bi-brightness-high-fill");
-            } else {
-                theme.setAttribute('href', lightTheme);
-                localStorage.setItem("setTheme", "light");
-                document.getElementById('themeSwitch').classList.remove("bi-brightness-high-fill");
-                document.getElementById('themeSwitch').classList.add("bi-moon-fill");
-                document.getElementById('colThemeSwitch').classList.remove("bi-brightness-high-fill");
-                document.getElementById('colThemeSwitch').classList.add("bi-moon-fill");
-            }
-        }
     function toggleTheme() {
         if (theme.getAttribute('href') == lightTheme) {
             theme.setAttribute('href', darkTheme);
