0e18a2d190101e3222771632162e8881df7cca86
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2021-12-02 23:25:42 +0000

    update

--- Resources/JS/base.js
+++ Resources/JS/base.js
@@ -309,8 +309,24 @@ const darkTheme = '/Resources/CSS/theme/dark.css';
                     document.getElementById('themeSwitch').classList.add("bi-moon-fill");
                     document.getElementById('colThemeSwitch').classList.add("bi-moon-fill");
                 } else {
-                    document.getElementById('themeSwitch').classList.add("bi-moon-fill");
-                    document.getElementById('colThemeSwitch').classList.add("bi-moon-fill");
+                    if (window.matchMedia('prefers-color-scheme: dark').matches) {
+                        link.setAttribute('href', darkTheme);
+                        localStorage.setItem("setTheme", "dark");
+                        document.getElementById('themeSwitch').classList.remove("bi-moon-fill");
+                        document.getElementById('themeSwitch').classList.add("bi-brightness-high-fill");
+                        document.getElementById('colThemeSwitch').classList.remove("bi-moon-fill");
+                        document.getElementById('colThemeSwitch').classList.add("bi-brightness-high-fill");
+                    } else if (window.matchMedia('prefers-color-scheme: light').matches) {
+                        link.setAttribute('href', lightTheme);
+                        localStorage.setItem("setTheme", "light");
+                        document.getElementById('themeSwitch').classList.remove("bi-brightness-high-fill");
+                        document.getElementById('themeSwitch').classList.add("bi-moon-fill");
+                        document.getElementById('colThemeSwitch').classList.remove("bi-brightness-high-fill");
+                        document.getElementById('colThemeSwitch').classList.add("bi-moon-fill");
+                    } else {
+                        document.getElementById('themeSwitch').classList.add("bi-moon-fill");
+                        document.getElementById('colThemeSwitch').classList.add("bi-moon-fill");
+                    }
                 }
             }
         }
