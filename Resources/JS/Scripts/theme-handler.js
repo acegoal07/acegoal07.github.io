09b036b11a06b71bd279c879b40a95409f1a2dd2
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2021-12-04 20:09:15 +0000

    update

--- Resources/JS/Scripts/theme-handler.js
+++ Resources/JS/Scripts/theme-handler.js
@@ -22,7 +22,7 @@ const darkTheme = '/Resources/CSS/theme/dark.css';
         }
         document.head.appendChild(link);
         document.onreadystatechange = () => {
-            if (document.readyState === 'loading') {
+            if (document.readyState === 'complete') {
                 document.getElementById('base').classList.remove('disable-transitions');
                 if (currentTheme === "dark") {
                     document.getElementById('themeSwitch').classList.add("bi-brightness-high-fill");
