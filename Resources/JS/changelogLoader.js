12186d21d8f132922b827e8bddcb8acd5f17ecb4
Alexander wood <acegoal07@gmail.com>
2022-03-23 10:31:00 +0000

    Update changelogLoader.js

--- Resources/JS/changelogLoader.js
+++ Resources/JS/changelogLoader.js
@@ -11,58 +11,27 @@ $.getJSON("/projects/sierra/json/changelog.json",
          for (changes of info.changes) {
             changesArray.push(`<li><p>${changes}</p></li>`);
          }
+         // Card
+         var card = 
+            `<div class="card">
+               <div class="card-header rounded-top border-top border-left border-right">
+                  Version ${info.version}
+               </div>
+               <div class="card-body rounded-bottom border-bottom border-left border-right">
+                  <h5 class="card-title">Changes</h5>
+                  <p class="card-text">
+                     <ul>
+                        ${changesArray.join('')}
+                     </ul>
+                  </p>
+               </div>
+            </div><br>`
          // Create current
-         if (count == 1) {
-            currentVersion =
-            `<div class="card">
-               <div class="card-header rounded-top border-top border-left border-right">
-                  Version ${info.version}
-               </div>
-               <div class="card-body rounded-bottom border-bottom border-left border-right">
-                  <h5 class="card-title">Changes</h5>
-                  <p class="card-text">
-                     <ul>
-                        ${changesArray.join('')}
-                     </ul>
-                  </p>
-               </div>
-            </div><br>`
-         }
+         if (count == 1) {currentVersion = card;}
          // Create previous
-         else if (count == 2) {
-            previousVersion =
-            `<div class="card">
-               <div class="card-header rounded-top border-top border-left border-right">
-                  Version ${info.version}
-               </div>
-               <div class="card-body rounded-bottom border-bottom border-left border-right">
-                  <h5 class="card-title">Changes</h5>
-                  <p class="card-text">
-                     <ul>
-                        ${changesArray.join('')}
-                     </ul>
-                  </p>
-               </div>
-            </div><br>`
-         }
+         else if (count == 2) {previousVersion = card;}
          // Create all old
-         else {
-            pastVersion.push(
-               `<div class="card">
-                  <div class="card-header rounded-top border-top border-left border-right">
-                     Version ${info.version}
-                  </div>
-                  <div class="card-body rounded-bottom border-bottom border-left border-right">
-                     <h5 class="card-title">Changes</h5>
-                     <p class="card-text">
-                        <ul>
-                           ${changesArray.join('')}
-                        </ul>
-                     </p>
-                  </div>
-               </div><br>`
-            )
-         }
+         else {pastVersion.push(card)}
       }
       // Set current
       const currentSet = document.getElementById('currentVersion');
