93b32a822e24ddc87821264885c29a49ba149ed0
acegoal07 <acegoal07@gmail.com>
2023-03-26 23:13:40 +0100

    Update themeHandler.js

--- resources/js/themeHandler.js
+++ resources/js/themeHandler.js
@@ -1,20 +1,24 @@
 // ThemeEngine ////// Version: 5.0 ////// By acegoal07 (can be found on twitter) //////////////////////////////////////////
 // On page load check for settings and system default
-if (localStorage.getItem("setTheme") === null) {
+
+const html = document.querySelector("html");
+const themeStorage = localStorage.getItem("setTheme");
+
+if (themeStorage === null) {
    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
       localStorage.setItem("setTheme", "dark");
-      document.querySelector("html").classList.replace("lightmode", "darkmode");
+      html.classList.replace("lightmode", "darkmode");
    } else {
       localStorage.setItem("setTheme", "light");
    }
-} else if (localStorage.getItem("setTheme") === 'dark') {
-   document.querySelector("html").classList.replace("lightmode","darkmode");
+} else if (themeStorage === 'dark') {
+   html.classList.replace("lightmode","darkmode");
 } else {void(0);}
 
-console.log(document.querySelector("html"));
+console.log(html);
 // Wait for page to finish loading to set buttons
 document.onloadstart = () => {
-   if (localStorage.getItem("setTheme") === "dark") {
+   if (themeStorage === "dark") {
       document.querySelectorAll(".themeButton").forEach(button => {
          button.classList.add("bi-brightness-high-fill");
       });
@@ -26,8 +30,8 @@ document.onloadstart = () => {
 }
 // document.onreadystatechange = () => {
 //    // If not 404 page set buttons
-//    if (document.readyState === "interactive" && !document.querySelector("html").classList.contains("error-page")) {
-//       if (localStorage.getItem("setTheme") === "dark") {
+//    if (document.readyState === "interactive" && !html.classList.contains("error-page")) {
+//       if (themeStorage === "dark") {
 //          document.querySelectorAll(".themeButton").forEach(button => {
 //             button.classList.add("bi-brightness-high-fill");
 //          });
@@ -41,35 +45,35 @@ document.onloadstart = () => {
 // Watch out for system default changes and adjust to it
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', theme => {
    if (theme.matches) {
-      document.querySelector("html").classList.replace("darkmode", "lightmode");
+      html.classList.replace("darkmode", "lightmode");
       localStorage.setItem("setTheme", "dark");
       // If not 404 page set buttons
-      if (!document.querySelector("html").classList.contains("error-page")) document.querySelectorAll(".themeButton").forEach(button => {
+      if (!html.classList.contains("error-page")) document.querySelectorAll(".themeButton").forEach(button => {
          button.classList.replace("bi-moon-fill", "bi-brightness-high-fill");
       });
    } else {
-      document.querySelector("html").classList.replace("lightmode", "darkmode");
+      html.classList.replace("lightmode", "darkmode");
       localStorage.setItem("setTheme", "light");
       // If not 404 page set buttons
-      if (!document.querySelector("html").classList.contains("error-page")) document.querySelectorAll(".themeButton").forEach(button => {
+      if (!html.classList.contains("error-page")) document.querySelectorAll(".themeButton").forEach(button => {
          button.classList.replace("bi-brightness-high-fill", "bi-moon-fill");
       });
    }
 });
 // Button function to change theme
 function themeButtonFunction() {
-   if (localStorage.getItem("setTheme") === "light") {
-      document.querySelector("html").classList.replace("lightmode", "darkmode");
+   if (themeStorage === "light") {
+      html.classList.replace("lightmode", "darkmode");
       localStorage.setItem("setTheme", "dark");
       // If not 404 page set buttons
-      if (!document.querySelector("html").classList.contains("error-page")) document.querySelectorAll(".themeButton").forEach(button => {
+      if (!html.classList.contains("error-page")) document.querySelectorAll(".themeButton").forEach(button => {
          button.classList.replace("bi-moon-fill", "bi-brightness-high-fill");
       });
    } else {
-      document.querySelector("html").classList.replace("darkmode", "lightmode");
+      html.classList.replace("darkmode", "lightmode");
       localStorage.setItem("setTheme", "light");
       // If not 404 page set buttons
-      if (!document.querySelector("html").classList.contains("error-page")) document.querySelectorAll(".themeButton").forEach(button => {
+      if (!html.classList.contains("error-page")) document.querySelectorAll(".themeButton").forEach(button => {
          button.classList.replace("bi-brightness-high-fill", "bi-moon-fill");
       });
    }
