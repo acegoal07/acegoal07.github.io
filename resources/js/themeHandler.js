173d29d7ae1fcc3334ed6c4d794c27483981373c
acegoal07 <acegoal07@gmail.com>
2023-03-26 23:15:34 +0100

    Update themeHandler.js

--- resources/js/themeHandler.js
+++ resources/js/themeHandler.js
@@ -17,31 +17,20 @@ if (themeStorage === null) {
 
 console.log(html);
 // Wait for page to finish loading to set buttons
-document.onloadstart = () => {
-   if (themeStorage === "dark") {
-      document.querySelectorAll(".themeButton").forEach(button => {
-         button.classList.add("bi-brightness-high-fill");
-      });
-   } else {
-      document.querySelectorAll(".themeButton").forEach(button => {
-         button.classList.add("bi-moon-fill");
-      });
+document.onreadystatechange = () =>  {
+   // If not 404 page set buttons
+   if (document.readyState === "interactive" && !html.classList.contains("error-page")) {
+      if (themeStorage === "dark") {
+         document.querySelectorAll(".themeButton").forEach(button => {
+            button.classList.add("bi-brightness-high-fill");
+         });
+      } else {
+         document.querySelectorAll(".themeButton").forEach(button => {
+            button.classList.add("bi-moon-fill");
+         });
+      }
    }
 }
-// document.onreadystatechange = () => {
-//    // If not 404 page set buttons
-//    if (document.readyState === "interactive" && !html.classList.contains("error-page")) {
-//       if (themeStorage === "dark") {
-//          document.querySelectorAll(".themeButton").forEach(button => {
-//             button.classList.add("bi-brightness-high-fill");
-//          });
-//       } else {
-//          document.querySelectorAll(".themeButton").forEach(button => {
-//             button.classList.add("bi-moon-fill");
-//          });
-//       }
-//    }
-// }
 // Watch out for system default changes and adjust to it
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', theme => {
    if (theme.matches) {
