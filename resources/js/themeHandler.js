4c0eaf897e0911791f70ae08aebd498e12b80495
acegoal07 <acegoal07@gmail.com>
2023-03-26 13:34:42 +0100

    Update themeHandler.js

--- resources/js/themeHandler.js
+++ resources/js/themeHandler.js
@@ -11,21 +11,31 @@ if (localStorage.getItem("setTheme") === null) {
    document.querySelector("html").classList.replace("lightmode","darkmode");
 } else {void(0);}
 // Wait for page to finish loading to set buttons
-document.onreadystatechange = () => {
-   // If not 404 page set buttons
-   if (document.readyState === "complete" && !document.querySelector("html").classList.contains("error-page")) {
-      if (localStorage.getItem("setTheme") === "dark") {
-         document.querySelectorAll(".themeButton").forEach(button => {
-            button.classList.add("bi-brightness-high-fill");
-         });
-      } else {
-         document.querySelectorAll(".themeButton").forEach(button => {
-            button.classList.add("bi-moon-fill");
-         });
-      }
-      document.querySelector("html").classList.add("transition");
+document.onloadstart = () => {
+   if (localStorage.getItem("setTheme") === "dark") {
+      document.querySelectorAll(".themeButton").forEach(button => {
+         button.classList.add("bi-brightness-high-fill");
+      });
+   } else {
+      document.querySelectorAll(".themeButton").forEach(button => {
+         button.classList.add("bi-moon-fill");
+      });
    }
 }
+// document.onreadystatechange = () => {
+//    // If not 404 page set buttons
+//    if (document.readyState === "interactive" && !document.querySelector("html").classList.contains("error-page")) {
+//       if (localStorage.getItem("setTheme") === "dark") {
+//          document.querySelectorAll(".themeButton").forEach(button => {
+//             button.classList.add("bi-brightness-high-fill");
+//          });
+//       } else {
+//          document.querySelectorAll(".themeButton").forEach(button => {
+//             button.classList.add("bi-moon-fill");
+//          });
+//       }
+//    }
+// }
 // Watch out for system default changes and adjust to it
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', theme => {
    if (theme.matches) {
