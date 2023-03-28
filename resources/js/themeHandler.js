d0bf726944b07ceb18d957a3203b38fd36139a09
acegoal07 <acegoal07@gmail.com>
2023-03-28 13:02:40 +0100

    update

--- resources/js/themeHandler.js
+++ resources/js/themeHandler.js
@@ -2,10 +2,10 @@
 // On page load check for settings and system default
 const html = document.querySelector("html");
 // Wait for page to finish loading to set buttons
-document.addEventListener("DOMContentLoaded", () => {
+document.onreadystatechange = () => {
    // If not 404 page set buttons
    if (document.readyState === "interactive" && !html.classList.contains("error-page")) {
-      if (localStorage.getItem("theme") === "dark") {
+      if (localStorage.getItem("theme") === "darkmode") {
          document.querySelectorAll(".themeButton").forEach(button => {
             button.classList.add("bi-brightness-high-fill");
          });
@@ -15,19 +15,19 @@ document.addEventListener("DOMContentLoaded", () => {
          });
       }
    }
-})
+}
 // Watch out for system default changes and adjust to it
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', theme => {
    if (theme.matches) {
-      document.documentElement.setAttribute("data-theme", "light");
-      localStorage.setItem("theme", "dark");
+      html.classList.replace("darkmode", "lightmode");
+      localStorage.setItem("theme", "darkmode");
       // If not 404 page set buttons
       if (!html.classList.contains("error-page")) document.querySelectorAll(".themeButton").forEach(button => {
          button.classList.replace("bi-moon-fill", "bi-brightness-high-fill");
       });
    } else {
-      document.documentElement.setAttribute("data-theme", "dark");
-      localStorage.setItem("theme", "light");
+      html.classList.replace("lightmode", "darkmode");
+      localStorage.setItem("theme", "lightmode");
       // If not 404 page set buttons
       if (!html.classList.contains("error-page")) document.querySelectorAll(".themeButton").forEach(button => {
          button.classList.replace("bi-brightness-high-fill", "bi-moon-fill");
@@ -36,16 +36,16 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', the
 });
 // Button function to change theme
 function themeButtonFunction() {
-   if (localStorage.getItem("theme") === "light") {
-      document.documentElement.setAttribute("data-theme", "dark");
-      localStorage.setItem("theme", "dark");
+   if (localStorage.getItem("theme") === "lightmode") {
+      html.classList.replace("lightmode", "darkmode");
+      localStorage.setItem("theme", "darkmode");
       // If not 404 page set buttons
       if (!html.classList.contains("error-page")) document.querySelectorAll(".themeButton").forEach(button => {
          button.classList.replace("bi-moon-fill", "bi-brightness-high-fill");
       });
    } else {
-      document.documentElement.setAttribute("data-theme", "light");
-      localStorage.setItem("theme", "light");
+      html.classList.replace("darkmode", "lightmode");
+      localStorage.setItem("theme", "lightmode");
       // If not 404 page set buttons
       if (!html.classList.contains("error-page")) document.querySelectorAll(".themeButton").forEach(button => {
          button.classList.replace("bi-brightness-high-fill", "bi-moon-fill");
