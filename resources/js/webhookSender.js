ade39f6b5f64091b80c5d88862f436dbb350520b
acegoal07 <acegoal07@gmail.com>
2023-09-06 17:36:26 +0100

    update

---
+++ resources/js/webhookSender.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", () => {
+   const date = new Date();
+   const fileData = [
+      `Date: ${date.toLocaleDateString("en-UK")}\n`,
+      `Time: ${date.toLocaleTimeString("en-UK")}\n`
+   ];
+   var formData = new FormData();
+   formData.append("file", new File(fileData, `Issue-${date.toLocaleString("en-uk")}.txt`, {type: "text/plain"}));
+   formData.append("content", "New issue reported");
+   const xml = new XMLHttpRequest();
+   xml.open("POST", process.env.ISSUE_WEBHOOK_URL);
+   xml.send(formData);
+}); 
+
