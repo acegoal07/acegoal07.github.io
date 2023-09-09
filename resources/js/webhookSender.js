777373dec076b3a6dbb287763ee3b16fdf517b02
acegoal07 <acegoal07@gmail.com>
2023-09-09 13:06:59 +0100

    update

--- resources/js/webhookSender.js
+++ resources/js/webhookSender.js
@@ -1,13 +1,15 @@
-function send() {
-   const date = new Date();
-   const fileData = [
-      `Date: ${date.toLocaleDateString("en-UK")}\n`,
-      `Time: ${date.toLocaleTimeString("en-UK")}\n`
-   ];
-   var formData = new FormData();
-   formData.append("file", new File(fileData, `Issue-${date.toLocaleString("en-uk")}.txt`, {type: "text/plain"}));
-   formData.append("content", "New issue reported");
-   const xml = new XMLHttpRequest();
-   xml.open("POST", `${import.meta.env.ISSUE_WEBHOOK_URL}`);
-   xml.send(formData);
-}; 
+window.addEventListener("DOMContentLoaded", () => {
+   document.querySelector("#test").addEventListener("click", () => {
+      const date = new Date();
+      const fileData = [
+         `Date: ${date.toLocaleDateString("en-UK")}\n`,
+         `Time: ${date.toLocaleTimeString("en-UK")}\n`
+      ];
+      var formData = new FormData();
+      formData.append("file", new File(fileData, `Issue-${date.toLocaleString("en-uk")}.txt`, {type: "text/plain"}));
+      formData.append("content", "New issue reported");
+      const xml = new XMLHttpRequest();
+      xml.open("POST", `${import.meta.env.ISSUE_WEBHOOK_URL}`);
+      xml.send(formData);
+   });
+}); 
