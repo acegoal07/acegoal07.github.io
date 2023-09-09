892c95f72f7d58b1a33fe4b0ff81dd589e30a247
acegoal07 <acegoal07@gmail.com>
2023-09-09 13:08:16 +0100

    Update webhookSender.js

--- resources/js/webhookSender.js
+++ resources/js/webhookSender.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
    document.querySelector("#test").addEventListener("click", () => {
       const date = new Date();
       const fileData = [
